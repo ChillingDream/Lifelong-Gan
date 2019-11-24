5427982841e491f51be261c2e386d3c2bd54c998
Ziheng_Li <1652961256@qq.com>
2019-11-24 16:00:58 +0800

    a version with bug

--- loader.py
+++ loader.py
@@ -6,93 +6,25 @@ import scipy.misc
 from scipy.misc import imread, imresize
 
 
-def load_images():
-    train_all = glob.glob("cityscapes/train/*.jpg")
-    train_img_A = []
-    train_img_B = []
+def load_images(dataset_dir, option):
+	all_imgs = glob.glob(dataset_dir + "/" + option + "/" + "*.jpg")
+	img_array_A = []
+	img_array_B = []
 
-    for file in train_all:
-        full_image = imread(file)
-        img_A = full_image[:, :full_image.shape[1] // 2, :]
-        img_B = full_image[:, full_image.shape[1] // 2:, :]
-        train_img_A.append(img_A)
-        train_img_B.append(img_B)
+	for file in all_imgs:
+		full_image = imread(file)
+		img_B = full_image[:, :full_image.shape[1] // 2, :]
+		img_A = full_image[:, full_image.shape[1] // 2:, :]
+		img_array_A.append(img_A)
+		img_array_B.append(img_B)
 
-    train_A = np.asarray(train_img_A)
-    train_B = np.asarray(train_img_B)
-    # print(train_A.shape)
-    #print(train_B.shape)
+	img_array_A = np.asarray(img_array_A) / 255
+	img_array_B = np.asarray(img_array_B) / 255
+	# print(train_A.shape)
+	#print(train_B.shape)
 
-    test_all = glob.glob("cityscapes/val/*.jpg")
-    test_img_A = []
-    test_img_B = []
+	return (img_array_A, img_array_B)
 
-    for file in test_all:
-        full_image = imread(file)
-        img_A = full_image[:, :full_image.shape[1] // 2, :]
-        img_B = full_image[:, full_image.shape[1] // 2:, :]
-        test_img_A.append(img_A)
-        test_img_B.append(img_B)
-
-    test_A = np.asarray(test_img_A)
-    test_B = np.asarray(test_img_B)
-    # print test_A.shape
-    # print test_B.shape
-
-    return train_A, train_B, test_A, test_B
-
-
-train_all = glob.glob("cityscapes/train/*.jpg")
-batch_size = 1
-
-
-def load_batch_image(idx):
-    full_image = imread(train_all[idx])
-    img_A = np.asarray(full_image[:, :full_image.shape[1] // 2, :]) / 255.
-    img_B = np.asarray(full_image[:, full_image.shape[1] // 2:, :]) / 255.
-    return img_A, img_B
-
-
-test_all = glob.glob("cityscapes/val/*.jpg")
-
-
-def load_test_image(idx):
-    full_image = imread(train_all[idx])
-    img_A = np.asarray(full_image[:, :full_image.shape[1] // 2, :]) / 255.
-    return img_A
-
-
-def save_images(image, size, img_path):
-    return imsave(inverse_transform(image), size, img_path)
-
-
-def imsave(image, img_size, img_path):
-    image = np.squeeze(merge(image, img_size))
-    return scipy.misc.imsave(img_path, image)
-
-
-def inverse_transform(image):
-    return (image + 1.) / 2.
-
-
-def merge(images, size):
-    h, w = images.shape[1], images.shape[2]
-    if (images.shape[3] in (3, 4)):
-        c = images.shape[3]
-        img = np.zeros((h * size[0], w * size[1], c))
-        for idx, image in enumerate(images):
-            i = idx % size[1]
-            j = idx // size[1]
-            img[j * h:j * h + h, i * w:i * w + w, :] = image
-        return img
-    elif images.shape[3] == 1:
-        img = np.zeros((h * size[0], w * size[1]))
-        for idx, image in enumerate(images):
-            i = idx % size[1]
-            j = idx // size[1]
-            img[j * h:j * h + h, i * w:i * w + w] = image[:, :, 0]
-            return img
-    else:
-        raise ValueError(
-            'In merge function, the first argument must have dimensions: HxW or HxWx3 or HxWx4'
-        )
+if __name__ == "__main__":
+	train_data = load_images("cityscapes", "train")
+	print(np.shape(train_data[1]))
--- train.py
+++ train.py
@@ -1,11 +1,10 @@
 import tensorflow as tf
+import time
 import numpy as np
-import model
 import tensorlayer as tl
-import loader
-import glob
+import os
 from model import Generator, Encoder, Discriminator
-from loader import load_batch_image, load_test_image, load_images
+from loader import load_images
 
 epochs = 20
 batch_size = 1
@@ -32,88 +31,89 @@ D = Discriminator((batch_size, 256, 256, 3))
 E = Encoder((batch_size, 256, 256, 3), Z_dim)
 opt = tf.optimizers.Adam(lrC, beta1)
 
-load_images()
 G.train()
 D.train()
 E.train()
-train_A = len(glob.glob('cityscapes/train/*.jpg'))
-num_batches = train_A // batch_size
+train_cityscapes = load_images("cityscapes", "train")
 
-image_A, image_B = load_batch_image(1)
-image_A = np.expand_dims(image_A, axis=0)
-image_B = np.expand_dims(image_B, axis=0)
-image_A = image_A.astype(np.float32)
-image_B = image_B.astype(np.float32)
-z = np.random.normal(size=(batch_size, Z_dim))
-z = z.astype(np.float32)
-encoded_true_img, encoded_mu, encoded_log_sigma = E(image_B)
-z = tf.reshape(z, z_shape0)
-z = tf.tile(z, z_shape1)
-encoded_true_img = tf.reshape(encoded_true_img, z_shape0)
-encoded_true_img = tf.tile(encoded_true_img, z_shape1)
-encoded_GI = tf.concat([image_A, encoded_true_img], axis=3)
-GI = tf.concat([image_A, z], axis=3)
-desired_gen_img = G(encoded_GI)
-LR_desired_img = G(GI)
+def train_one_task(train_data, use_aux_data = False):
+	start_time = time.time()
+	for epoch in range(epochs):
+		tot_loss = []
+		processed = 0
+		for image_A, image_B in tl.iterate.minibatches(train_data[0], train_data[1], batch_size, shuffle=False):
+			z = np.random.normal(size=(batch_size, Z_dim))
+			image_A = image_A.astype(np.float32)
+			image_B = image_B.astype(np.float32)
+			z = z.astype(np.float32)
 
-for epoch in range(epochs):
-    for idx in range(train_A):
-        image_A, image_B = load_batch_image(idx)
-        image_A = np.expand_dims(image_A, axis=0)
-        image_B = np.expand_dims(image_B, axis=0)
-        z = np.random.normal(size=(batch_size, Z_dim))
-        image_A = image_A.astype(np.float32)
-        image_B = image_B.astype(np.float32)
-        z = z.astype(np.float32)
+			with tf.GradientTape(persistent=True) as tape:
+				encoded_true_img, encoded_mu, encoded_log_sigma = E(image_B)
 
-        with tf.GradientTape(persistent=True) as tape:
-            encoded_true_img, encoded_mu, encoded_log_sigma = E(image_B)
+				z = tf.reshape(z, z_shape0)
+				z = tf.tile(z, z_shape1)
+				encoded_true_img = tf.reshape(encoded_true_img, z_shape0)
+				encoded_true_img = tf.tile(encoded_true_img, z_shape1)
+				encoded_GI = tf.concat([image_A, encoded_true_img], axis=3)
+				GI = tf.concat([image_A, z], axis=3)
+				desired_gen_img = G(encoded_GI)
+				LR_desired_img = G(GI)
 
-            z = tf.reshape(z, z_shape0)
-            z = tf.tile(z, z_shape1)
-            encoded_true_img = tf.reshape(encoded_true_img, z_shape0)
-            encoded_true_img = tf.tile(encoded_true_img, z_shape1)
-            encoded_GI = tf.concat([image_A, encoded_true_img], axis=3)
-            GI = tf.concat([image_A, z], axis=3)
-            desired_gen_img = G(encoded_GI)
-            LR_desired_img = G(GI)
+				reconst_z, reconst_mu, reconst_log_sigma = E(LR_desired_img)
 
-            reconst_z, reconst_mu, reconst_log_sigma = E(LR_desired_img)
+				P_real = D(image_B)
+				P_fake = D(LR_desired_img)
+				P_fake_encoded = D(desired_gen_img)
 
-            P_real = D(image_B)
-            P_fake = D(LR_desired_img)
-            P_fake_encoded = D(desired_gen_img)
+				loss_vae_D = (tl.cost.sigmoid_cross_entropy(P_real, tf.ones_like(P_real)) +
+							  tl.cost.sigmoid_cross_entropy(P_fake_encoded, tf.zeros_like(P_fake_encoded)))
+				loss_lr_D = (tl.cost.sigmoid_cross_entropy(P_real, tf.ones_like(P_real)) +
+							 tl.cost.sigmoid_cross_entropy(P_fake, tf.zeros_like(P_fake)))
+				loss_vae_G = tl.cost.sigmoid_cross_entropy(P_fake_encoded, tf.ones_like(P_fake_encoded))
+				loss_G = tl.cost.sigmoid_cross_entropy(P_fake, tf.ones_like(P_fake))
+				loss_vae_L1 = tl.cost.absolute_difference_error(
+					image_B, desired_gen_img, axis=[-1, -2])
+				loss_latent_GE = tl.cost.absolute_difference_error(z, reconst_z)
+				loss_kl_E = 0.5 * tf.reduce_mean(
+					-1 - 2 * encoded_log_sigma + encoded_mu**2 +
+					tf.exp(2 * encoded_log_sigma))
 
-            loss_vae_D = (
-				tl.cost.mean_squared_error(P_real, 0.9) +
-                tl.cost.mean_squared_error(P_fake_encoded, 0))
-            loss_lr_D = (
-                tl.cost.mean_squared_error(P_real, 0.9) +
-                tl.cost.mean_squared_error(P_fake, 0))
-            loss_vae_GE = tl.cost.mean_squared_error(P_fake_encoded, 0.9)
-            loss_G = tl.cost.mean_squared_error(P_fake, 0.9)
-            loss_vae_GE = tf.metrics.mean_absolute_error(image_B, desired_gen_img)
-            loss_latent_GE = tf.metrics.mean_absolute_error(z, reconst_z)
-            loss_kl_E = 0.5 * tf.reduce_mean(
-                -1 - encoded_log_sigma + encoded_mu**2 +
-                tf.exp(encoded_log_sigma))
+				loss_D = loss_vae_D + loss_lr_D
+				loss_G = loss_vae_G + loss_G + reconst_C * loss_vae_L1 + latent_C * loss_latent_GE
+				loss_E = loss_vae_G + reconst_C * loss_vae_L1 + latent_C * loss_latent_GE + kl_C * loss_kl_E
+				tot_loss.append([loss_G, loss_D, loss_E])
 
-            loss_D = loss_vae_D + loss_lr_D - tl.cost.mean_squared_error(P_real, 0.9)
-            loss_G = loss_vae_GE + loss_G + reconst_C * loss_vae_GE + latent_C * loss_latent_GE
-            loss_E = loss_vae_GE + reconst_C * loss_vae_GE + latent_C * loss_latent_GE + kl_C * loss_kl_E
+			grad = tape.gradient(loss_G, G.trainable_weights)
+			opt.apply_gradients(zip(grad, G.trainable_weights))
+			grad = tape.gradient(loss_D, D.trainable_weights)
+			opt.apply_gradients(zip(grad, D.trainable_weights))
+			grad = tape.gradient(loss_E, E.trainable_weights)
+			opt.apply_gradients(zip(grad, E.trainable_weights))
+			del tape
+			processed += batch_size
+			tot_loss = np.mean(tot_loss, 0)
+			print("time_used=%f" % (time.time() - start_time))
+			print("Epoch %d finished! loss_G=%f loss_D=%f loss_E=%f" % (epoch, tot_loss[0], tot_loss[1], tot_loss[2]))
+			tot_loss = []
+			if processed > 100:
+				print("#", end="")
+				processed -= 100
 
-        grad = tape.gradient(loss_G, G.trainable_weights)
-        opt.apply_gradients(zip(grad, G.trainable_weights))
-        grad = tape.gradient(loss_D, D.trainable_weights)
-        opt.apply_gradients(zip(grad, D.trainable_weights))
-        grad = tape.gradient(loss_E, E.trainable_weights)
-        opt.apply_gradients(zip(grad, E.trainable_weights))
+		print("")
+		if epoch % 1 == 0:
+			tot_loss = np.mean(tot_loss, 0)
+			print("time_used=%f" % (time.time() - start_time))
+			print("Epoch %d finished! loss_G=%f loss_D=%f loss_E=%f" % (epoch, tot_loss[0], tot_loss[1], tot_loss[2]))
+			'''
+			tl.vis.save_images(desired_gen_img.numpy(), [1, 1],
+							   os.path.join(save_dir,
+											'vae_g_{}.png'.format(epoch)))
+			tl.vis.save_images(LR_desired_img.numpy(), [1, 1],
+							   os.path.join(save_dir,
+											'lr_g_{}.png'.format(epoch)))
+											'''
 
-    if epoch != 0 and epoch % 10 == 0:
-        tl.vis.save_images(desired_gen_img.numpy(), [2, 4],
-                           os.path.join(save_dir, 'vae_g_{}.png'.format(epoch)))
-        tl.vis.save_images(LR_desired_img.numpy(), [2, 4],
-                           os.path.join(save_dir, 'lr_g_{}.png'.format(epoch)))
+train_one_task(train_cityscapes, None)
 
 #G.eval()
 #out=G(valid_...).numpy()
